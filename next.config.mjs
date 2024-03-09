053764934c75c9751b7a92ec2c672f23cd627a83
cheolminJOO <cheolmin.joo@gmail.com>
2024-03-09 20:05:07 +0900

    transpilePackages 속성 next.confg.mjs 파일에 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    'antd',
+    '@ant-design',
+    'rc-util',
+    'kitchen-flow-editor',
+    '@ant-design/pro-editor',
+    'zustand',
+    'leva',
+    'rc-pagination',
+    'rc-picker',
+    'rc-notification',
+    'rc-tooltip',
+    'react-daum-postcode',
+    'react-hook-form',
+    'sweetalert2',
+  ],
 };
 
 export default nextConfig;
-
-
