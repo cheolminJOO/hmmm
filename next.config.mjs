dd9bc40729cf6067f6d4eca41ab628840488308b
cheolminJOO <cheolmin.joo@gmail.com>
2024-03-09 20:25:15 +0900

    테스트 종료

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,18 @@ const nextConfig = {
   transpilePackages: [
     'antd',
     '@ant-design',
+    'rc-util',
+    'kitchen-flow-editor',
     '@ant-design/pro-editor',
-
+    'zustand',
+    'leva',
+    'rc-pagination',
+    'rc-picker',
+    'rc-notification',
+    'rc-tooltip',
+    'react-daum-postcode',
+    'react-hook-form',
+    'sweetalert2',
   ],
 };
 
